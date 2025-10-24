13ef382fd32683cb2901222bad795d178f685311
meyers-kim <114023472+meyers-kim@users.noreply.github.com>
2025-10-24 14:19:51 +0200

    room exports file

---
+++ escaperoom/rooms/__init__.py
@@ -0,0 +1,16 @@
+# just export rooms so i can import them easily
+from .intro import IntroRoom
+from .soc import SocRoom
+from .dns import DnsRoom
+from .vault import VaultRoom
+from .malware import MalwareRoom
+from .final import FinalGateRoom
+
+__all__ = [
+    "IntroRoom",
+    "SocRoom",
+    "DnsRoom",
+    "VaultRoom",
+    "MalwareRoom",
+    "FinalGateRoom",
+]
